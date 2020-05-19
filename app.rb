119ddcc2536c48bfd27c9c5f8618ebbd6086201c
Adewunmi Awolesi <adewunmi.awolesi@beis.gov.uk>
2020-05-19 16:07:55 +0100

    chapter 10 error

--- app.rb
+++ app.rb
@@ -8,8 +8,19 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks' do
     @bookmarks = Bookmark.all
-    erb :bookmarks
+    erb :"bookmarks/index"
   end
+
+  get 'bookmarks/new' do
+    erb :"new"
+  end
+
+  post '/bookmarks' do
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    redirect '/bookmarks'
+  end 
 
   run! if app_file == $0
 end
